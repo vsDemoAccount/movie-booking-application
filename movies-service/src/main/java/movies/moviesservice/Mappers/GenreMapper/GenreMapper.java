d219eee47f0938c5965a876c2c8e8525c2b99fe8
sachin acc <atkaresachin470@gmail.com>
2025-11-16 18:58:35 +0530

    created a gener controller

--- movies-service/src/main/java/movies/moviesservice/Mappers/GenreMapper/GenreMapper.java
+++ movies-service/src/main/java/movies/moviesservice/Mappers/GenreMapper/GenreMapper.java
@@ -21,6 +21,7 @@ public class GenreMapper {
     public GenreDto toDto(Genre entity) {
         if (entity == null) return null;
         return GenreDto.builder()
+                .name(entity.getName())
                 .code(entity.getCode())
                 .build();
     }
--- movies-service/src/main/java/movies/moviesservice/controller/GenreController/GenreController.java
+++ movies-service/src/main/java/movies/moviesservice/controller/GenreController/GenreController.java
@@ -6,6 +6,7 @@ import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
 import movies.moviesservice.dtos.GenreDto.GenreDto;
 import movies.moviesservice.services.GenreServiceImpl.GenreServiceImpl;
+import movies.moviesservice.utils.ApiResponse;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
@@ -19,9 +20,14 @@ public class GenreController {
     private final GenreServiceImpl service;
 
     @PostMapping
+    public ResponseEntity<ApiResponse<GenreDto>> create(@Valid @RequestBody GenreDto dto) {
         GenreDto response = service.create(dto);
+        return ResponseEntity.status(HttpStatus.CREATED).body(ApiResponse.ok("Genre created", response));
     }
 
     @GetMapping
+    public ResponseEntity<ApiResponse<List<GenreDto>>> getAllCodes() {
+        List<GenreDto> list = service.getAllCodes();
+        return ResponseEntity.ok(ApiResponse.ok("Genres retrieved", list));
     }
 }

62fbde80d64576222e29989473832a3cfcafb900
sachin acc <atkaresachin470@gmail.com>
2025-12-02 20:57:08 +0530

    created a Theatre service

---
+++ theatre-service/src/test/java/movies/theatreservice/TheatreServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package movies.theatreservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TheatreServiceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
